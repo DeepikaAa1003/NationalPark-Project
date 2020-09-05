e716b6197cf3badac3664808461643411d903d76
Mlara <64501664+Mlara73@users.noreply.github.com>
2020-09-05 16:18:43 -0500

    Retrieving web camps and campgrounds, and store it within the MOngoDB

--- 01.NP_ETL.py
+++ 01.NP_ETL.py
@@ -22,15 +22,18 @@ db.parks.drop()
 # open/create parks collection
 parks = db.parks
 
-#API Retrieval Process
+# API Retrieval Process
 
-#Parks Collection insert
+#### Parks Retrieval ####
+
+# Parks Collection insert
 park_base_url = "https://developer.nps.gov/api/v1/parks?"
 
 counter = 0
 while (counter <= 450):
     with open('parks.json', 'w') as f:   
         park_url= f"{park_base_url}&api_key={np_api_key}&start={counter}"
+        print(park_url)
         response_json = requests.get(park_url).json()
         json.dump(response_json, f)
 
@@ -44,7 +47,7 @@ print(counter)
 
 #### Web Cams Retrieval ####
 
-# Drops parks collection if available to remove duplicates
+# Drops webcams collection if available to remove duplicates
 db.webcams.drop()
 
 # open/create parks collection
@@ -67,3 +70,30 @@ while (cam_counter <= 150):
             print("CamData Inserted")
             cam_counter = cam_counter + 1
 print(cam_counter)
+
+
+#### CampGrounds Retrieval ####
+
+# Drops campgrounds collection if available to remove duplicates
+db.campgrounds.drop()
+
+# open/create campgrounds collection
+campgrounds = db.campgrounds
+
+# Web Cams Collection insert
+camps_url = "https://developer.nps.gov/api/v1/campgrounds?"
+
+camps_counter = 0
+while (camps_counter <= 500):
+    with open('campgrounds.json', 'w') as f:   
+        campgrounds_url= f"{camps_url}&api_key={np_api_key}&start={camps_counter}"
+        camps_response_json = requests.get(campgrounds_url).json()
+        json.dump(camps_response_json, f)
+
+        camps_data = camps_response_json['data']
+    
+        for camp in camps_data:
+            campgrounds.insert_one(camp)
+            print("CampGrounds Inserted")
+            camps_counter = camps_counter + 1
+print(camps_counter)
