b223f7b24db4bae6f20a898f6f450c66196ab420
unknown <deepika1003@yahoo.com>
2020-09-12 16:47:21 -0500

    changed config file

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-connect_string = "mongodb+srv://ProjectNPSParks:rUnrZ7ZjLJVXyT6@cluster0.za3mz.mongodb.net?retryWrites=true&w=majority"
+connect_string = "mongodb+srv://ProjectNPSParks:rUnrZ7ZjLJVXyT6@cluster0.za3mz.mongodb.net/?retryWrites=true&w=majority"
