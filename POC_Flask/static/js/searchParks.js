8d7231d8017e126f966f398753b2bc9f4ac5f5fd
unknown <deepika1003@yahoo.com>
2020-09-12 12:05:32 -0500

    Loadded leaflet map with markers to show all search results

--- POC_Flask/static/js/searchParks.js
+++ POC_Flask/static/js/searchParks.js
@@ -1,34 +1,59 @@
+
+
 let selectedParkName = "", selectedState = "", selectedActivity = "";
 let searchParksButton = d3.select("#park");
-// selectedParkName = d3.select("#selParkName").value;
-// selectedActivity = d3.select("#selActivity").value;
-// selectedState = d3.select("#selStates").value;
-// selectedState = "";
-// selectedParkName = "";
-// selectedActivity = "";
 let searchCriteria = "";
 let tbody = d3.select("#searchResults");
+let parks = [];
+
+ // Create a map object
+ const myMap = L.map("mapid", {
+    center: [37.09, -95.71],
+    zoom: 5
+});
 
 function statechanged(state) {
-    // Fetch new data each time a new sample is selected
+    // Fetch new data each time a new state is selected
     selectedState = state;
   
 }
 function activitychanged(activity) {
-// Fetch new data each time a new sample is selected
+// Fetch new data each time a new activity is selected
     selectedActivity = activity;
 
 }
 function parkchanged(parkname) {
-// Fetch new data each time a new sample is selected
+// Fetch new data each time a new parkname is selected
     selectedParkName = parkname;
 
 }
-function searchParks(event){
-
-
+function loadParkMap(parks){
+   
+    console.log("========================Inside map function");
+  
+    // Add a tile layer
+    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/streets-v11",
+      accessToken: API_KEY
+    }).addTo(myMap);
     
-    // console.log("Searching Parks");
+    console.log(parks);
+    // Loop through the cities array and create one marker for each city, bind a popup containing its name and population add it to the map
+    parks.forEach(coord => {
+        L.marker(coord)
+        .addTo(myMap);
+    })
+    
+    
+  }
+
+function searchParks(event){
+    tbody.innerHTML = "";
+    // Find out which drop down values are selected and decide the selection criteria
     let searchCriteria;
     let searchParametersList = [];
     console.log(selectedState);
@@ -79,11 +104,14 @@ function searchParks(event){
     console.log(searchCriteria);
     console.log(searchParametersList);
     ExtractData(searchCriteria, searchParametersList);
-
+    console.log("Calling LoadMap");
+    // console.log(parks);
+    loadParkMap(parks);
+    
 }
-  
+
 function ExtractData(searchCriteria, searchParametersList){
-    // console.log("inside extractdata");
+    // Prepare the URL to get the data for selected criteria and parameters from Mongo DB
     let dataUrl = `/parks/v1.0/${searchCriteria}`;
 
 
@@ -91,11 +119,21 @@ function ExtractData(searchCriteria, searchParametersList){
         dataUrl = `${dataUrl}/${search}`;
     });
     console.log(dataUrl);
-       
+    let parkLat, parkLon, templocation;
     d3.json(dataUrl).then((data) => {
-        // console.log(data);
+        
         data.forEach((park) => {
-             console.log(park);
+             //console.log(park);
+            //   parkLat = parseFloat(park["latitude"]);
+            //   parkLon = parseFloat(park["longitude"]);
+            //   templocation = {};
+            //   //console.log(temp);
+            //   templocation["location"] = [parkLat, parkLon];
+            //   console.log(templocation);
+            //   parks.push(templocation);
+
+            parks.push([parseFloat(park.latitude),parseFloat(park.longitude)])
+             
             var row = tbody.append("tr");
                 var cell = row.append("td");
                 cell.html(park["fullName"]);
@@ -114,10 +152,14 @@ function ExtractData(searchCriteria, searchParametersList){
                 
                 
         });
+        
     });
-       
+    
+    
     
 }
-  // Use D3 `.on` to attach a click handler for the upvote
+
+
+  // Use D3 `.on` to attach a click handler
   searchParksButton.on("click", searchParks);
   
